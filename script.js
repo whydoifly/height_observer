95d6c98bdc3f6402803d1507973dc096bbbd0028
a.cheprasov <a.cheprasov@intermeda.ru>
2021-12-29 11:35:51 +0300

    ready

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // Сюда передать название класса контейнера, с которым необходимо взаимодействовать
 let elToCheck = document.querySelector('.work-container');
 
+
+let elDataHeightNumber = Number(elToCheck.dataset.height);
+let elDataHeightString = elDataHeightNumber + 'px';
+
 // Кнопка 'Показать больше'
-let expandBtn = document.createElement('a');
+let expandBtn = document.createElement('i');
     expandBtn.classList.add('expand-desc-btn');
     expandBtn.textContent = 'Показать больше';
+    expandBtn.style.position = 'absolute';
+    expandBtn.style.zIndex = '100';
+    expandBtn.style.bottom = '0';
 
 
 // Кнопка 'Скрыть описание'
-let collapseBtn = document.createElement('a');
+let collapseBtn = document.createElement('i');
     collapseBtn.classList.add('collapse-desc-btn');
     collapseBtn.textContent = 'Скрыть описание';
 
@@ -16,35 +23,35 @@ let collapseBtn = document.createElement('a');
 expandBtn.addEventListener('click', () => {
     if (document.querySelector('.expand-gradient')) {
         document.querySelector('.expand-gradient').classList.remove('expand-gradient');
-        removeButton(elToCheck, expandBtn);
+        removeButton(expandBtn);
         addButton(elToCheck, collapseBtn);
+        elToCheck.style.height = "100%";
     }
 });
 
 collapseBtn.addEventListener('click', () => {
     if (document.querySelector('.collapse-desc-btn')) {
         elToCheck.classList.add('expand-gradient');
-        elToCheck.insertAdjacentElement('afterEnd', expandBtn);
+        elToCheck.insertAdjacentElement('beforeend', expandBtn);
+        elToCheck.style.height = elDataHeightString;
     }
 });
 
+heightChecker(elToCheck);
 
 function heightChecker(element) {
-    let elDataHeightNumber = Number(element.dataset.height);
-    let elDataHeightString = elDataHeightNumber + 'px';
+    
     if (element.offsetHeight > elDataHeightNumber) {
         element.style.height = elDataHeightString;
         element.classList.add('expand-gradient');
-        element.insertAdjacentElement('afterEnd', expandBtn);
+        element.insertAdjacentElement('beforeend', expandBtn);
     }
 }
 
-function removeButton(div, button) {
-    div.parentNode.removeChild(button);
+function removeButton(button) {
+    button.remove();
 }
 
 function addButton(div, button) {
     div.insertAdjacentElement('beforeend', button)
 }
-
-heightChecker(elToCheck);
